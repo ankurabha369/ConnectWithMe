2b4734c851502b7007bec915149b34b5574c8417
Ankur Rabha <165087787+ankurabha369@users.noreply.github.com>
2025-01-25 16:41:58 +0530

    updation

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
   container.classList.add("visible");
 
   // Add event listeners to social links for bounce effect
-  const socialLinks = document.querySelectorAll(".social-links div");
-  socialLinks.forEach((link) => {
-    link.addEventListener("mouseenter", () => {
-      link.style.transform = "scale(1.2)";
-    });
-    link.addEventListener("mouseleave", () => {
-      link.style.transform = "scale(1)";
-    });
-  });
+  // const socialLinks = document.querySelectorAll(".social-links div");
+  // socialLinks.forEach((link) => {
+  //   link.addEventListener("mouseenter", () => {
+  //     link.style.transform = "scale(1.2)";
+  //   });
+  //   link.addEventListener("mouseleave", () => {
+  //     link.style.transform = "scale(1)";
+  //   });
+  // });
 });
